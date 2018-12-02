c931280cb11c3bba624118aa06b8b24ee391c036
Linus <wendefelix1998@gmail.com>
2018-12-02 06:36:17 +0100

    Optimized performance for part 2 and added comments

--- puzzle2.py
+++ puzzle2.py
@@ -1,12 +1,15 @@
+from itertools import combinations
+
+
 def solve_part_1(puzzle_input: str):
     excactlyTwoOfAnyKind, excatlyThreeOfAnyKind = 0, 0
 
     for boxId in puzzle_input.split('\n'):
-        letterCount = dict()
+        letterCount = dict()  # e.g.: {'a': 1, 'b': 1, 'c': 2, ... }
         for letter in boxId:
             letterCount[letter] = letterCount.get(letter, 0) + 1
         
-        letterCountValues = letterCount.values()
+        letterCountValues = letterCount.values()  # Contains only the counts of same letters
         if 2 in letterCountValues:
             excactlyTwoOfAnyKind += 1
         if 3 in letterCountValues:
@@ -16,24 +19,17 @@ def solve_part_1(puzzle_input: str):
 
 
 def solve_part_2(puzzle_input: str):
-    boxIds = puzzle_input.split('\n')
+    for firstBoxId, secondBoxId in combinations(puzzle_input.split('\n'), 2):
 
-    for firstBoxId in boxIds:
-        for secondBoxId in boxIds:
-            if firstBoxId == secondBoxId:
-                continue
-
-            differingLetters = 0
-            for firstIdLetter, secondIdLetter in zip(firstBoxId, secondBoxId):
-                if firstIdLetter != secondIdLetter:
-                    differingLetters += 1
-                
-                if differingLetters > 1:
-                    break
-            else:
-                # FOUND!
-                commonCharacters = ''
-                for firstIdLetter, secondIdLetter in zip(firstBoxId, secondBoxId):
-                    if firstIdLetter == secondIdLetter:
-                        commonCharacters += firstIdLetter
-                return commonCharacters
+        # Check if more than 1 differing letter is contained
+        differingLetters = 0
+        for firstIdLetter, secondIdLetter in zip(firstBoxId, secondBoxId):
+            if firstIdLetter != secondIdLetter:
+                differingLetters += 1
+            
+            if differingLetters > 1:
+                break
+        else:
+            # FOUND!
+            # Return only same letters:
+            return str().join(letter1 for letter1, letter2 in zip(firstBoxId, secondBoxId) if letter1 == letter2)
