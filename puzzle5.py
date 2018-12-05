61f8349d67fb17771621c73fa0b984281e1fc977
Linus <wendefelix1998@gmail.com>
2018-12-05 20:28:04 +0100

    Commented, refactored and slightly optimized code.
    The performance was already good, but pypy3 made the results worse on this puzzle!

--- puzzle5.py
+++ puzzle5.py
@@ -1,25 +1,26 @@
-
-def oppsite_polarity(a, b):
-    """Returns True when a and b are lower and upper case or vice versa."""
-    return abs(ord(a) - ord(b)) == 32  # 32 decimal places apart in ASCII chart
-
-
-def react_polymers(polymerList):
-    """Counts polymers, returns the length of the final polymerList"""
+def react_polymers(polymerList: list) -> int:
+    """
+    Removes polymers of opposite polarity and returns the size.
+    The given list 'polymerList' will get modified!
+    """
     
     index = 0
     polymerSize = len(polymerList)  # Should stay same as 'len(polymerList)'
-    
+
+    # Reducing all opposite polarities:
     while index < (polymerSize-1):
         if index < 0:
             index = 0
         
-        a, b = polymerList[index], polymerList[index + 1]
-        
-        if oppsite_polarity(a, b):
+        # There is no performance difference in using this rather than comparing ord()-values
+        polyA, polyB = polymerList[index], polymerList[index + 1]
+        # Check for opposite polarity:
+        if polyA != polyB and polyA.lower() == polyB.lower():
             polymerList.pop(index)
             polymerList.pop(index)
             polymerSize -= 2
+            # Go back one before this is removed to check
+            # if a new pair was created that can get removed:
             index -= 2
 
         index += 1
@@ -28,10 +29,10 @@ def react_polymers(polymerList):
 
 
 def solve_part_1(puzzle_input: str):
-    polymerList = list(puzzle_input)
-    return react_polymers(polymerList)
+    return react_polymers(list(puzzle_input))
 
 def solve_part_2(puzzle_input: str):
+    # Find all possible polymers (not the complete alphabet is used)
     possiblePolymers = set()
     for polymer in puzzle_input:
         if not polymer == polymer.lower():
@@ -39,11 +40,15 @@ def solve_part_2(puzzle_input: str):
         if polymer not in possiblePolymers:
             possiblePolymers.add(polymer)
 
+    # Check every polymerList without a certain polymer
+    # and look for the best size:
     bestPolymerSize = None
     
     for excludedPolymer in possiblePolymers:
         polmerList = list(puzzle_input.replace(excludedPolymer, '').replace(excludedPolymer.upper(), ''))
         polymerSize = react_polymers(polmerList)
+
+        # Check if this run was better:
         if bestPolymerSize is None or polymerSize < bestPolymerSize:
             bestPolymerSize = polymerSize
     
